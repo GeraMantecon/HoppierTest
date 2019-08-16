0501cda6de06d450b37fe0a4e7f431858e92eef1
GeraMantecon <gerardo.mantecon@gmail.com>
2019-08-16 12:12:59 -0400

    Second commit to make compatible with both python 2 and 3

--- hoppier_test.py
+++ hoppier_test.py
@@ -3,7 +3,7 @@ import json
 
 #Method to transform and clean string with snacks to list of snacks.
 def to_SnackList(snack_string):
-    snacks = [snack.encode('utf-8').strip() for snack in snack_string.replace(' and ',',').split(',')]
+    snacks = [snack.strip() for snack in snack_string.replace(' and ',',').split(',')]
     return snacks
 
 def main():
@@ -17,18 +17,18 @@ def main():
     emailsSnacks = [(snacker['email'], to_SnackList(snacker['fave_snack'])) for snacker in snackersData if 'fave_snack' in snacker]
     #Obtaine dictionary of snacks in stock (assuming they are all in stock) with their price. 
     #using dictionary becuases we are going to look the price by product.
-    products = dict([(product['title'].encode('utf-8'),product['variants'][0]['price']) for product in productsData['products']])
+    products = dict([(product['title'],product['variants'][0]['price']) for product in productsData['products']])
     #Find all snacker's fave snacks that we have
     snackerSnack = [(snacker[0], snack) for snacker in emailsSnacks for snack in snacker[1] if snack in products.keys()]
     
     #A)
     print('List the real stocked snacks you found under the snacker\'s \'fave_snack\'?')
-    for snack in list(set(zip(*snackerSnack)[1])):
+    for snack in list(set(list(zip(*snackerSnack))[1])):
         print(snack)
     #B)
     print('\n'+'What\'re the emails of the snackers who listed those as a \'fave_snack\'?')
-    for email in zip(*snackerSnack)[0]:
-        print(email.encode('utf-8'))
+    for email in list(zip(*snackerSnack))[0]:
+        print(email)
     #C)
     print('\n'+'If all those snackers we\'re to pay for their \'fave_snack\' what\'s the total price?')
     print(sum(float(products[snack]) for snacker,snack in snackerSnack))
